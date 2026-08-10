99b14c1b04c7da766a096c5ecc877c5f7d560a1a
norve11z <norvezac30@gmail.com>
2026-08-10 15:43:01 -0500

    fixed pcr ids

--- lib/dakboard.ts
+++ lib/dakboard.ts
@@ -1,19 +1,19 @@
 const DAKBOARD_DISPLAYS = [
   {
     deviceId: "dev_c02d8e5131cd",
-    screenId: "615b33af-105a44-9b96-e14c9ba82588",
+    screenId: "scr_e14c9ba82588",
   },
   {
     deviceId: "dev_349de5a3fc05",
-    screenId: "618c2105-105a44-3284-23dbcc3b51b8",
+    screenId: "scr_23dbcc3b51b8",
   },
   {
     deviceId: "dev_a896f7fd81f7",
-    screenId: "615b33fe-105a44-cad4-116052e55442",
+    screenId: "scr_116052e55442",
   },
   {
     deviceId: "dev_c7f4a05a4350",
-    screenId: "624c8fe8-105a44-f55a-72dbdeaa56ca",
+    screenId: "scr_72dbdeaa56ca",
   },
 ];
 
